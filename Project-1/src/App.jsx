1a7d148eb975a1cd904d463d229c6a40c669ec97
Muhammad Sajid <sajidmsd775@gmail.com>
2023-12-31 00:36:16 +0500

    Basics Of React

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+
+      <h1>Basics of React</h1>
+      <p>at this stage i have been completed the basic of react that how React work , how the code is change to java script , how a variable can be printed in the return statement like we write the variable name in curly braces as {anyname}, how the components manage in root main.jsx, and at last what is JSX_java script  xml  </p>
+
+    </>
+  )
+}
+
+export default App
